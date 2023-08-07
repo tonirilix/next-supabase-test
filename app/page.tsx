4e7f6e22eef7c301702fd112a6ec496e04cbe812
Jesus Montes <3756508+tonirilix@users.noreply.github.com>
2023-08-06 19:50:46 -0600

    feat(tweet) add like button

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import { cookies } from "next/headers";
 import AuthButtonServer from "./auth-button-server";
 import { redirect } from "next/navigation";
 import { NewTweet } from "./new-tweet";
+import { Likes } from "./likes";
 
 export default async function Home() {
   const supabase = createServerComponentClient<SupabaseDB>({ cookies });
@@ -15,18 +16,28 @@ export default async function Home() {
     redirect("/login");
   }
 
-  const { data: tweets, error } = await supabase
+  const { data, error } = await supabase
     .from("tweets")
-    .select("*, profiles(*)");
+    .select("*, profiles(*), likes(*)");
+
+  const tweets =
+    data?.map((tweet) => ({
+      ...tweet,
+      user_has_liked_tweet: !!tweet.likes.find(
+        (like) => like.user_id === session.user.id
+      ),
+      likes: tweet.likes.length,
+    })) ?? [];
 
   return (
     <>
       <AuthButtonServer />
       <NewTweet />
-      {tweets?.map((tweet, index) => (
+      {tweets?.map((tweet) => (
         <div key={tweet.id}>
-          <p>{tweet.profiles?.username}</p>
+          <p>{tweet?.profiles?.username}</p>
           <p>{tweet.title}</p>
+          <Likes tweet={tweet} />
         </div>
       ))}
     </>
