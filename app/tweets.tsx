4bce710de9ccb4c8f866b6ef4d9549986144dc8c
Jesus Montes <3756508+tonirilix@users.noreply.github.com>
2023-08-08 21:56:37 -0600

    feat(tweet) real time tweets - all events

--- app/tweets.tsx
+++ app/tweets.tsx
@@ -1,11 +1,14 @@
 "use client";
-import { experimental_useOptimistic as useOptimistic } from "react";
+import { useEffect, experimental_useOptimistic as useOptimistic } from "react";
 import { Likes } from "./likes";
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { useRouter } from "next/navigation";
 
 type TweetsProps = {
   tweets: Array<any>;
 };
 export function Tweets({ tweets }: TweetsProps) {
+  const router = useRouter();
   const [optimisticTweets, addOptimisticTweet] = useOptimistic<
     TweetsProps["tweets"],
     any
@@ -18,6 +21,30 @@ export function Tweets({ tweets }: TweetsProps) {
 
     return newOptimisticTweets;
   });
+
+  const supabase = createClientComponentClient<SupabaseDB>();
+  useEffect(() => {
+    const channel = supabase
+      .channel("realtime tweets")
+      .on(
+        "postgres_changes",
+        {
+          event: "*",
+          schema: "public",
+          table: "tweets",
+        },
+        (payload) => {
+          console.log("payload", payload);
+          router.refresh();
+        }
+      )
+      .subscribe();
+
+    return () => {
+      supabase.removeChannel(channel);
+    };
+  }, [router, supabase]);
+
   return optimisticTweets?.map((tweet) => (
     <div key={tweet.id}>
       <p>{tweet?.profiles?.username}</p>
